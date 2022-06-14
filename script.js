3b8cf049d725b5eeb7f67f480a2ac7559cbb58fe
Lucas Vinn <90799507+LucasVinn@users.noreply.github.com>
2022-06-13 22:12:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function changeColor() {
+    window.alert('Pare de me cutucar🔥')
+}
+
+
+// Scroll Reveal //
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.button',{duration: 1400});
+sr.reveal('.reveal', {duration: 2000});
+sr.reveal('.nebula', {duration: 2400})
+
+
